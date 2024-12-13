607c5f3905a2d0ee2dc694971806ad650431ae9a
sriramnatarajan <sriramnat123@gmail.com>
2024-12-13 02:05:35 -0600

    test

--- calling.py
+++ calling.py
@@ -2,7 +2,7 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 scope = "user-library-read"
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="8e17206030f14c2383d748fc59c4da01",
-                                               client_secret="bc989d63f7fb45faaa5f2fb48ffeb4cc",
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="",
+                                               client_secret="",
                                                redirect_uri="http://localhost",
                                                scope="user-library-read"))
